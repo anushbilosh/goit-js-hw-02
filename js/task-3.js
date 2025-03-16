ab20bc39e493bd4354de1bf13b4509827882a935
Anush Biloshytska <biloshytska.anush@icloud.com>
2025-03-16 21:41:33 +0200

    homework has done

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,13 @@
-function getElementWidth (content, padding, border) {
-   const result = parseFloat (content) + (2 * parseFloat(padding)) + (2 * parseFloat(border));
-    return result
+function checkForSpam(message) {
+    const banWord1 = message.toLowerCase().includes("spam");
+    const banWord2 = message.toLowerCase().includes("sale");
+    return banWord1 || banWord2 
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
